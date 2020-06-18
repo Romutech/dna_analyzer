125c984afd1eed4673d2e91d061a65ea946bd2dd
Robin Muller <robin.muller.romutech@gmail.com>
2020-06-18 14:10:44 +0200

    feature: installation and configuration of matplotlib

--- main/urls.py
+++ main/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('',views.home,name="home"),
+    path('a', views.index, name="index"),
     path('page/<int:page>', views.index, name="index"),
     path('create', views.create, name="create"),
     path('<int:id>', views.read, name="read"),
     path('update/<int:id>', views.update, name="update"),
     path('delete/<int:id>', views.delete, name="delete"),
     path('sequence/<int:id>/analyze', views.analyze, name="analyze"),
-]
+    ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,25 @@ from .models import *
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage
 
+from django.shortcuts import render
+from matplotlib import pyplot as plt
+import io
+import urllib, base64
+import matplotlib
+matplotlib.use('Agg')
+
+def home(request):
+    plt.plot(range(10))
+    fig = plt.gcf()
+    #convert graph into dtring buffer and then we convert 64 bit code into image
+    buf = io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri =  urllib.parse.quote(string)
+    return render(request,'main/home.html',{'data':uri})
+
+
 def index(request, page=1):
     sequences = Sequence.objects.order_by('-date')
     paginator = Paginator(sequences, 2, 1)
@@ -54,3 +73,4 @@ def analyze(request, id):
     sequence = get_object_or_404(Sequence, id=id)
     sequence.analyse()
     return redirect('read', id)
+
