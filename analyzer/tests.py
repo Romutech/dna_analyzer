633f38c61064559624d66bdb268f73b6c47a95a1
Robin Muller <robin.muller.romutech@gmail.com>
2020-07-03 03:06:20 +0200

    feature: test_number_nucleotides method of sequence class

--- analyzer/tests.py
+++ analyzer/tests.py
@@ -16,3 +16,12 @@ class SequenceTest(TestCase):
 
     def setUp(self):
         self.sequence = Sequence.objects.get(id=1)
+
+    def test_number_nucleotides(self):
+        self.sequence.number_nucleotides()
+        self.assertEqual(self.sequence.nb_bases, 40)
+        self.assertEqual(self.sequence.nb_a, 10)
+        self.assertEqual(self.sequence.nb_c, 10)
+        self.assertEqual(self.sequence.nb_g, 10)
+        self.assertEqual(self.sequence.nb_t, 10)
+
