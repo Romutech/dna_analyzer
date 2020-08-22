83a121d1c1da531c9e54473a98c77fdaf242eee5
Robin Muller <robin.muller.romutech@gmail.com>
2020-08-22 22:16:45 +0200

    feature: update method

--- analyzer/forms.py
+++ analyzer/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Sequence
 
 class SequenceForm(forms.Form):
-    title       = forms.CharField(max_length=100)
-    file_path   = forms.FileField()
-    note        = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(max_length=100)
+    file_path = forms.FileField(required= True)
+    note = forms.CharField(widget=forms.Textarea)
+
+class SequenceFormUpdate(forms.Form):
+    title = forms.CharField(max_length=100)
+    file_path = forms.FileField(required= False)
+    note = forms.CharField(widget=forms.Textarea)
--- analyzer/views.py
+++ analyzer/views.py
@@ -1,32 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
-from .forms import SequenceForm
+from .forms import SequenceForm, SequenceFormUpdate
 from .models import *
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage
 import requests
 from django.conf import settings
 import json
+import uuid
 
 def index(request, page=1):
-    # -----------------------------------------------
-    resp = requests.get(settings.URL())
-
-    sequences = resp.json()
-
-    paginator = Paginator(sequences, 20, 5)
-    try:
-        sequences = paginator.page(page)
-    except EmptyPage:
-        sequences = paginator.page(paginator.num_pages)
-    return render(request, 'analyzer/index.html', locals())
-
-
-    # -----------------------------------------------
-
     if not request.user.is_active:
         return redirect('user_login')
-    sequences = Sequence.objects.filter(user=request.user.id)
+
+    response = requests.get(settings.URL())
+    sequences = response.json()
     paginator = Paginator(sequences, 20, 5)
+
     try:
         sequences = paginator.page(page)
     except EmptyPage:
@@ -37,78 +26,101 @@ def index(request, page=1):
 def create(request):
     if not request.user.is_active:
         return redirect('user_login')
+
     if request.method == 'POST':
         form = SequenceForm(request.POST, request.FILES)
-        if form.is_valid():
-            # fichier = open('media/' + str(request.FILES['file_path'], 'wt')
-            # fichier.write(
-            #     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \nPellentesque gravida erat ut lectus convallis auctor. \nFusce mollis sem id tellus auctor hendrerit.")
-            # fichier.close()
 
+        if form.is_valid():
             file = request.FILES['file_path']
 
-            with open('media/' + str(request.FILES['file_path']) , 'wb+') as destination:
+            with open('media/' + str(file) , 'wb+') as genome_file:
                 for chunk in file.chunks():
-                    destination.write(chunk)
+                    genome_file.write(chunk)
 
-
-            with open('media/' + str(request.FILES['file_path']), 'r') as genome_file:
+            with open('media/' + str(file), 'r') as genome_file:
                 genome_file.readline()
+                file = genome_file.read()
 
-                json_data = {
-                    'title': request.POST['title'],
-                    'file_path': str(request.FILES['file_path']),
-                    'note': request.POST['note'],
-                    'user_id': request.user.id,
-                    'file': genome_file.read()
-                }
+            json_data = {
+                'title': request.POST['title'],
+                'file_path': str(request.FILES['file_path']),
+                'note': request.POST['note'],
+                'user_id': request.user.id,
+                'file': file
+            }
 
             requests.post(settings.URL(), json=json_data)
+            response = requests.get(settings.URL())
+            sequences = response.json()
+            paginator = Paginator(sequences, 20, 5)
 
-            return redirect('index')
+            try:
+                sequences = paginator.page(1)
+            except EmptyPage:
+                sequences = paginator.page(paginator.num_pages)
+            return render(request, 'analyzer/index.html', locals())
+            return render(request, 'analyzer/read.html', locals())
+
+
+
     form = SequenceForm()
     return render(request, 'analyzer/sequence_form.html', locals())
 
 
-def read(request, id):
-    # -----------------------------------------------
+def read(request, unique_id):
+    response = requests.get(settings.URL(unique_id))
 
-    resp = requests.get(settings.URL(id))
-
-    sequence = resp.json()
+    sequence = response.json()
 
     if sequence['nb_bases'] is not None:
         sequence.nb_bases = int(sequence.nb_bases)
     return render(request, 'analyzer/read.html', locals())
 
-    # -----------------------------------------------
 
+def update(request, unique_id):
     if not request.user.is_active:
         return redirect('user_login')
-    sequence = get_object_or_404(Sequence, id=id, user=request.user.id)
-    if sequence.nb_bases is not None:
-        sequence.nb_bases = int(sequence.nb_bases)
-    return render(request, 'analyzer/read.html', locals())
 
+    response = requests.get(settings.URL(unique_id))
 
-def update(request, id):
-    if not request.user.is_active:
-        return redirect('user_login')
-    sequence = get_object_or_404(Sequence, id=id, user=request.user.id)
+    sequence = response.json()
+
     if request.method == 'POST':
-        form = SequenceForm(request.POST, request.FILES, instance=sequence)
+        form = SequenceFormUpdate(request.POST, request.FILES)
         if form.is_valid():
-            form.save(request.user.id)
-            return redirect('read', id)
+            if 'file_path' in request.FILES:
+                file = request.FILES['file_path']
+
+                with open('media/' + str(file), 'wb+') as genome_file:
+                    for chunk in file.chunks():
+                        genome_file.write(chunk)
+
+                with open('media/' + str(file), 'r') as genome_file:
+                    genome_file.readline()
+                    file = genome_file.read()
+
+            json_data = {
+                'title': request.POST['title'],
+                'note': request.POST['note'],
+                'user_id': request.user.id,
+            }
+
+            if 'file' in locals():
+                json_data['file_path'] = str(request.FILES['file_path'])
+                json_data['file'] = file
+
+            requests.put(settings.URL(unique_id), json=json_data)
+
+            return redirect('read', unique_id)
     else:
-        form = SequenceForm(instance=sequence)
+        form = SequenceFormUpdate(sequence)
     return render(request, 'analyzer/sequence_form.html', locals())
 
 
-def delete(request, id):
+def delete(request, unique_id):
     if not request.user.is_active:
         return redirect('user_login')
-    sequence = get_object_or_404(Sequence, id=id, user=request.user.id)
+    sequence = get_object_or_404(Sequence, id=unique_id, user=request.user.id)
     sequence.delete()
     messages.add_message(request, messages.SUCCESS, 'La séquence ADN a bien été supprimée !')
     return redirect('index')
